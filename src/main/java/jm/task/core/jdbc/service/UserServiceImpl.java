666d4ba70f4504455852c1c54041c2f3893788dc
OccRx <ilippolitov@gmail.com>
2022-01-15 15:40:46 +0300

    test

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    //private UserDaoJDBCImpl us = new UserDaoJDBCImpl();
+//    private UserDaoJDBCImpl us = new UserDaoJDBCImpl();
     private UserDaoHibernateImpl us = new UserDaoHibernateImpl();
 
     public void createUsersTable() {
