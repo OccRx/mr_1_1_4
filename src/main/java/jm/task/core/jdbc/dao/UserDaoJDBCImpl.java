1781161c1b3e3a5d321068abd335bcbbbc061c59
OccRx <ilippolitov@gmail.com>
2022-01-18 22:23:35 +0300

    UserDaoJDBCImpl v1.2

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,7 +2,7 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,14 +14,19 @@ public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
 
     }
-    private void prepStatementSQL(String sql){
-        try(PreparedStatement pr = Util.getConnection().prepareStatement(sql)){
-            pr.execute();
+    private void prepStatementSQL(String sql) {
+        try (Connection connection = Util.getConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement pr = connection.prepareStatement(sql)) {
+                pr.execute();
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
     public void createUsersTable()  {
         String sql = "CREATE TABLE IF NOT EXISTS user" +
                 " (id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(25), lastName VARCHAR(25), age TINYINT)";
@@ -35,34 +40,43 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void saveUser(String name, String lastName, byte age) {
         String sqlSave = "INSERT INTO user (name, lastName, age) Values (?, ?, ?)";
-        try(PreparedStatement pr = Util.getConnection().prepareStatement(sqlSave)){
-            pr.setString(1, name);
-            pr.setString(2, lastName);
-            pr.setByte(3, age);
-            pr.execute();
-            System.out.printf("User с именем – %s добавлен в базу данных \n", name);
-        } catch (SQLException e) {
+        try(Connection connection = Util.getConnection()) {
+            connection.setAutoCommit(false);
+            try(PreparedStatement pr = connection.prepareStatement(sqlSave)){
+                pr.setString(1, name);
+                pr.setString(2, lastName);
+                pr.setByte(3, age);
+                pr.execute();
+                connection.commit();
+                System.out.printf("User с именем – %s добавлен в базу данных \n", name);
+            } catch (SQLException e) {
+                connection.rollback();
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     public void removeUserById(long id) {
-        String sqlRemove = "DELETE FROM user WHERE Id = id";
+        String sqlRemove = "DELETE FROM user WHERE Id = ?";
         prepStatementSQL(sqlRemove);
     }
 
     public List<User> getAllUsers() {
         String addList = "SELECT * FROM user";
         List<User> list = new ArrayList<>();
-        try(PreparedStatement pr = Util.getConnection().prepareStatement(addList);
-            ResultSet rs = pr.executeQuery()){
-            while (rs.next()){
-                User user = new User();
-                user.setId(rs.getLong("id"));
-                user.setName(rs.getString("name"));
-                user.setLastName(rs.getString("lastName"));
-                user.setAge(rs.getByte("age"));
-                list.add(user);
+        try (Connection connection = Util.getConnection()) {
+            try (PreparedStatement pr = connection.prepareStatement(addList);
+                ResultSet rs = pr.executeQuery()) {
+                while (rs.next()) {
+                    User user = new User();
+                    user.setId(rs.getLong("id"));
+                    user.setName(rs.getString("name"));
+                    user.setLastName(rs.getString("lastName"));
+                    user.setAge(rs.getByte("age"));
+                    list.add(user);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
